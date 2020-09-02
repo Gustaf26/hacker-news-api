ba761d4d75d91236f42d65eb6b2b4732d27394ba
gustavocsverdrup <gustavo.catalasverdrup@gmail.com>
2020-09-02 14:37:07 +0200

    adding initial results in main  page

--- src/components/HackerNewsLatest.js
+++ src/components/HackerNewsLatest.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
+import useFetch from '../hooks/useFetch'
 
 const HackerNewsLatest = () => {
+
+	const [query, setQuery] = useState('http://hn.algolia.com/api/v1/search?tags=front_page');
+	const [{ data, isLoading, error }, setUrl] = useFetch('');
+
+	useEffect(()=>{
+
+		setUrl(query)
+
+	},[])
+
+
+	const renderNews = hits => {
+
+		console.log(hits)
+		 return hits.map((article, index) => (
+		 	<li key={index} className="list-group-item">
+		 		<h3>{article.title}</h3>
+
+		 		<p className="text-muted small">
+		 			Posted at {article.created_at} by {article.author}
+		 		</p>
+
+		 		<p>
+		 			<Link to={{
+		 				pathname: `/articles/${article.objectID}`,
+		 				state: {
+	 						article
+		 				},
+		 			}} className="btn btn-sm btn-primary">Read more</Link>
+		 		</p>
+		 	</li>
+		 ))
+	}
+
+
 	return (
 		<>
 			<h2>This would show the latest articles</h2>
@@ -10,6 +46,8 @@ const HackerNewsLatest = () => {
 
 			<ul className="list-group">
 				<li className="list-group-item">
+
+				<div>
 					<h3>Some Article</h3>
 
 					<p className="text-muted small">
@@ -19,6 +57,13 @@ const HackerNewsLatest = () => {
 					<p>
 						<Link to='/articles/24336087' className="btn btn-sm btn-primary">Read more</Link>
 					</p>
+				</div>
+				<div>
+					{data.hits?
+					<ul className="search-results list-group">
+						{renderNews(data.hits)}
+					</ul>:null}
+				</div>
 				</li>
 			</ul>
 		</>
